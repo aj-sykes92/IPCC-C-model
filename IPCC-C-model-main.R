ac8caee6be231ecc373484e3236fbb9d8c7a7a2b
Alasdair Sykes <alasdair.sykes@gmail.com>
2019-11-04 17:07:06 +0000

    test

--- IPCC-C-model-main.R
+++ IPCC-C-model-main.R
@@ -3,7 +3,7 @@
 # load req'd packages
 library(tidyverse)
 
-# implement top-level function for C stock change
+# implement top-level function for C stock change 
 SOC_stock_change <- function(Active_y, Slow_y, Passive_y){
   SOC_y <- sum(c(Active_y, Slow_y, Passive_y))
   A_CMineral <- SOC_y - lag(SOC_y, default = 0)
