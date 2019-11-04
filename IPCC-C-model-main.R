79af0a2b687bd48cbf3c731223f2c2dc2748c055
Alasdair Sykes <alasdair.sykes@gmail.com>
2019-11-04 18:08:21 +0000

    Minor corrections on .csv and changed param assign to function to account for tillage practice.

--- IPCC-C-model-main.R
+++ IPCC-C-model-main.R
@@ -3,6 +3,7 @@
 # load req'd packages
 library(tidyverse)
 
+##################
 # implement top-level function for C stock change 
 SOC_stock_change <- function(Active_y, Slow_y, Passive_y){
   SOC_y <- sum(c(Active_y, Slow_y, Passive_y))
@@ -10,13 +11,27 @@ SOC_stock_change <- function(Active_y, Slow_y, Passive_y){
   return(A_CMineral)
 }
 
+##################
 # read in model parameters
 # source: https://www.ipcc-nggip.iges.or.jp/public/2019rf/index.html, Vol. 4 .zip folder
-# assign to objects
+# function to assign parameters to Global Environment objects
 Dat_param <- read_csv("Model-parameters.csv")
-for(i in 1:nrow(Dat_param)){
-  assign(Dat_param$Parameter[i], Dat_param$Default[i])
-}
-rm(i)
+bind_rows(Dat_param, tibble(Parameter = tillfac, ))
 
-# 
+assign_params <- function(till_type){
+  Dat_param <- Dat_param %>%
+    filter(Practice == till_type | Practice == "All") %>%
+    mutate(Parameter = Parameter %>% str_replace_all("_.+", ""))
+  
+  for(i in 1:nrow(Dat_param)){
+    assign(Dat_param$Parameter[i], Dat_param$Default[i])
+  }
+  rm(i)
+  
+}
+
+assign_params(till_type = "Full till")
+
+##################
+# algorithm to calculate active pool
+
