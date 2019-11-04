dc47e0d12387bacc75a20767566645a817fc172d
Alasdair Sykes <alasdair.sykes@gmail.com>
2019-11-04 17:22:08 +0000

    Added .csv w/ model parameters and read into environment.

--- IPCC-C-model-main.R
+++ IPCC-C-model-main.R
@@ -10,4 +10,13 @@ SOC_stock_change <- function(Active_y, Slow_y, Passive_y){
   return(A_CMineral)
 }
 
+# read in model parameters
+# source: https://www.ipcc-nggip.iges.or.jp/public/2019rf/index.html, Vol. 4 .zip folder
+# assign to objects
+Dat_param <- read_csv("Model-parameters.csv")
+for(i in 1:nrow(Dat_param)){
+  assign(Dat_param$Parameter[i], Dat_param$Default[i])
+}
+rm(i)
 
+# 
